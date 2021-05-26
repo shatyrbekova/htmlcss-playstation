01b19a76db738df1f36883fe13c6d683dea58006
shatyrbekova <perizat.shatyrbekova@gmail.com>
2021-05-26 16:15:27 +0200

    Js/alert

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-document.writeln ("Hello World!");
-document.writeln ("Hello from me!");
+//document.writeln ("Hello World!");
+//document.writeln ("Hello from me!");
+// console.log ("Hello from  a Boolenaer");
 
-console.log ("Hello from  a Boolenaer");
+document.getElementById ('myID').innerHTML = "Hello from a Booleaner!"; 
+
+alert('Accedi all app boolean.it con le credenziali Google')
